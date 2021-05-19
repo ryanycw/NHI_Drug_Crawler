61c3bb83b39fa114090c6848334fc9485ddf0a41
ryanycw <ryan0213.tem04@nctu.edu.tw>
2021-05-19 14:40:55 +0800

    finish crawling all feature

--- crawler.py
+++ crawler.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 import pandas as pd
@@ -14,9 +15,14 @@ class Drug:
         browser = webdriver.Chrome(ChromeDriverManager().install())
         browser.get("https://www.nhi.gov.tw/QueryN/Query1.aspx")
 
-        result = 'drug_id' + ',' + 'act_code'
+        result = 'drug_id' + ',' + 'drug_name_en' + ',' + 'drug_name_zh' + ',' + 'ingre' + ',' + 'ingre_unit' + ',' + \
+                 'stand_unit' + ',' + 'mixture' + ',' + 'sales' + ',' + 'form' + ',' + 'classify_code' + ',' + 'class_gr_name' + ',' + 'act_code'
+
         for drug_name in self.drug_names:
             result += '\n' + drug_name + ','
+            drug_name_en_list, drug_name_zh_list, ingre_list, ingre_unit_list, stand_unit_list, mixture_list, sales_list, form_list, \
+            classify_code_list, class_gr_name_list, act_code_list = set(), set(), set(), set(), set(), set(), set(), set(), set(), set(), set()
+            all_data = [drug_name_en_list, drug_name_zh_list, ingre_list, ingre_unit_list, stand_unit_list, mixture_list, sales_list, form_list, classify_code_list, class_gr_name_list, act_code_list]
             try:
                 print(drug_name)
                 drug = browser.find_element_by_xpath('//*[@id="ctl00_ContentPlaceHolder1_tbxQ1ID"]')
@@ -36,19 +42,52 @@ class Drug:
                 
                 soup = BeautifulSoup(browser.page_source, "lxml")
                 table = soup.find("table", {"id": "ctl00_ContentPlaceHolder1_gvQuery1Data"})
-
                 if table != None:
-                    elements = table.find_all("td", attrs={'style':'width:8%;'})
-                    act_code = set(element.getText() for element in elements)
+                    rows = table.find_all("tr")
+                    for i, row in enumerate(rows):
+                        if i != 0:
+                            drug_id = row.find("td", recursive=False).findNext("td")
+                            drug_name_en = drug_id.findNext("td")
+                            drug_name_en_list.add(drug_name_en.find("span").getText())
+                            drug_name_zh = drug_name_en.findNext("td")
+                            drug_name_zh_list.add(drug_name_zh.find("span").getText())
+                            ingre = drug_name_zh.findNext('td')
+                            ingre_list.add(ingre.getText().strip())
+                            ingre_unit = ingre.findNext('td')
+                            ingre_unit_list.add(ingre_unit.getText().strip())
+                            stand_unit = ingre_unit.findNext('td')
+                            stand_unit_list.add(stand_unit.find("span").getText())
+                            mixture = stand_unit.findNext('td')
+                            mixture_list.add(mixture.find("span").getText())
+                            sales = mixture.findNext('td').findNext('td').findNext('td')
+                            sales_list.add(sales.find("span").getText())
+                            form = sales.findNext('td')
+                            form_list.add(form.find("span").getText())
+                            classify_code = form.findNext('td')
+                            classify_code_list.add(classify_code.find("span").getText())
+                            class_gr_name = classify_code.findNext('td')
+                            class_gr_name_list.add(class_gr_name.find("span").getText())
+                            act_code = class_gr_name.findNext('td')
+                            act_code_list.add(act_code.getText())
 
-                    for code in act_code:
-                        result += code + ','
+                    for data in all_data:
+                        if len(data) == 1:
+                            result += "".join(map(str,data)) + ','
+                        else:
+                            text = ""
+                            for i in range(len(data)):
+                                text += " ".join(map(str,data))
+                            result += text + ','
 
                     result = result[:-1]
                 
             except Exception as e:
-                result += 'Not Found'
-                print(e)
+                if e == TimeoutException:
+                    print(e)
+                    result += 'Time Out'
+                else:
+                    print(e)
+                    result += 'Not Found'
 
         return result
 
